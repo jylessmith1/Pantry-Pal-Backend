5df434ad93acb629c5d9df213fb56d091155b1d8
kblack-git <keithblack@gmail.com>
2023-06-08 14:59:07 -0400

    .

--- routes/inventory.js
+++ routes/inventory.js
@@ -5,17 +5,17 @@ const { inventoryItem } = require('../models');
 
 router.post('/', async (req, res) => {
     try {
-        const newItem = await message.create(req.body);
+        const newItem = await inventoryItem.create(req.body);
         res.status(201).json(newItem);
       } catch (error) {
         res.status(500).json({ message: 'Error adding inventory item', error });
       }
     })
 
-    router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
         try {
             //This will need to be modified to .findALl( where: item.belongsToId = the user Id)
-            const items = await shopping_list.findAll();
+            const items = await inventoryItem.findAll();
             res.json(items);
         } catch (error) {
             res.status(500).json({ message: "Error retrieving shopping list items", error });
