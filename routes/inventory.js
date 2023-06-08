24a9063c8a2a2df1eb1773eb87d01b6b29774609
kblack-git <keithblack@gmail.com>
2023-06-08 14:51:03 -0400

    .

--- routes/inventory.js
+++ routes/inventory.js
@@ -10,6 +10,16 @@ router.post('/', async (req, res) => {
       } catch (error) {
         res.status(500).json({ message: 'Error adding inventory item', error });
       }
-    });
+    })
+
+    router.get('/', async (req, res) => {
+        try {
+            //This will need to be modified to .findALl( where: item.belongsToId = the user Id)
+            const items = await shopping_list.findAll();
+            res.json(items);
+        } catch (error) {
+            res.status(500).json({ message: "Error retrieving shopping list items", error });
+        }
+    })
 
     module.exports = router;
