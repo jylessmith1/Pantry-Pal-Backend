ffefa11e178467cc1570f558ad200e4e77f8b797
kblack-git <keithblack@gmail.com>
2023-06-09 10:41:37 -0400

    git and post working

--- routes/shopping_list.js
+++ routes/shopping_list.js
@@ -13,4 +13,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        //This will need to be modified to .findALl( where: item.belongsToId = the user Id)
+        const items = await shopping_list.create();
+        let stuff = [{ "id": 1, "name": "from the top rope", "image": "banana.jpg" }, { "id": 2, "name": "Randy Orton", "image": "steak.jpg" }]
+        res.json(items);
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving shopping list items", error });
+    }
+})
+
 module.exports = router;
